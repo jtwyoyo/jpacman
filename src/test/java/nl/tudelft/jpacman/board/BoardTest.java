9d63ba25df36aceb7ea0afc32d28b509a0d64061
YoYo <jittiwatyoyo@gmail.com>
2023-09-20 18:43:12 +0700

    fix: remove unused import statement

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -1,10 +1,8 @@
 package nl.tudelft.jpacman.board;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 /**
